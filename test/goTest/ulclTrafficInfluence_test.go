69d29857e98ac14356b25a97b9555668c39c08ed
Alonza0314 <dufengplus@gmail.com>
2025-02-24 13:26:04 +0000

    refactor: extract test func

--- test/goTest/ulclTrafficInfluence_test.go
+++ test/goTest/ulclTrafficInfluence_test.go
@@ -12,78 +12,69 @@ import (
 )
 
 func TestULCLTrafficInfluence(t *testing.T) {
-	var (
-		err    error
-		output []byte
-		cmd    *exec.Cmd
-		pr     *packetRusher.PacketRusher
-	)
-
-	// activate PacketRusher
-	pr = packetRusher.NewPacketRusher()
+	// PacketRusher
+	pr := packetRusher.NewPacketRusher()
 	pr.Activate()
+	defer pr.Deactivate()
 
 	time.Sleep(3 * time.Second)
 
 	// before TI
 	t.Run("Before TI", func(t *testing.T) {
-		err = pinger.Pinger(N6GW_IP, NIC)
-		if err != nil {
-			t.Errorf("Ping n6gw failed: expected ping success, but got %v", err)
-		}
-		err = pinger.Pinger(MEC_IP, NIC)
-		if err == nil {
-			t.Errorf("Ping mec failed: expected ping failed, but got %v", err)
-		}
+		pingN6gwSuccessMecFailed(t)
 	})
 
 	// post TI
-	cmd = exec.Command("bash", "../api-udr-ti-data-action.sh", "put")
-	output, err = cmd.CombinedOutput()
-	if err != nil {
-		t.Errorf("Insert ti data failed: expected insert success, but got %v, output: %s", err, output)
-	}
-	time.Sleep(3 * time.Millisecond)
+	tiOperation(t, "put")
 
 	// after TI
 	t.Run("After TI", func(t *testing.T) {
-		err = pinger.Pinger(N6GW_IP, NIC)
-		if err != nil {
-			t.Errorf("Ping n6gw failed: expected ping failed, but got %v", err)
-		}
-		err = pinger.Pinger(MEC_IP, NIC)
-		if err == nil {
-			t.Errorf("Ping mec failed: expected ping sucess, but got %v", err)
-		}
+		pingN6gwFailedMecSuccess(t)
 	})
 
 	// delete TI
-	cmd = exec.Command("bash", "../api-udr-ti-data-action.sh", "delete")
-	output, err = cmd.CombinedOutput()
-	if err != nil {
-		t.Errorf("Delete ti data failed: expected delete success, but got %v, output: %s", err, output)
-	}
-	time.Sleep(3 * time.Millisecond)
+	tiOperation(t, "delete")
 
 	// reset TI
 	t.Run("Reset TI", func(t *testing.T) {
-		err = pinger.Pinger(N6GW_IP, NIC)
-		if err != nil {
-			t.Errorf("Ping n6gw failed: expected ping success, but got %v", err)
-		}
-		err = pinger.Pinger(MEC_IP, NIC)
-		if err == nil {
-			t.Errorf("Ping mec failed: expected ping failed, but got %v", err)
-		}
+		pingN6gwSuccessMecFailed(t)
 	})
 
 	// check charging record
 	t.Run("Check Charging Record", func(t *testing.T) {
 		checkChargingRecord(t)
 	})
+}
 
-	// deactivate PacketRusher
-	pr.Deactivate()
+func pingN6gwSuccessMecFailed(t *testing.T) {
+	err := pinger.Pinger(N6GW_IP, NIC)
+	if err != nil {
+		t.Errorf("Ping n6gw failed: expected ping success, but got %v", err)
+	}
+	err = pinger.Pinger(MEC_IP, NIC)
+	if err == nil {
+		t.Errorf("Ping mec failed: expected ping failed, but got %v", err)
+	}
+}
+
+func pingN6gwFailedMecSuccess(t *testing.T) {
+	err := pinger.Pinger(N6GW_IP, NIC)
+	if err != nil {
+		t.Errorf("Ping n6gw failed: expected ping failed, but got %v", err)
+	}
+	err = pinger.Pinger(MEC_IP, NIC)
+	if err == nil {
+		t.Errorf("Ping mec failed: expected ping sucess, but got %v", err)
+	}
+}
+
+func tiOperation(t *testing.T, operation string) {
+	cmd := exec.Command("bash", "../api-udr-ti-data-action.sh", operation)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Errorf("TI operation failed: expected %s success, but got %v, output: %s", operation, err, output)
+	}
+	time.Sleep(3 * time.Millisecond)
 }
 
 func checkChargingRecord(t *testing.T) {
