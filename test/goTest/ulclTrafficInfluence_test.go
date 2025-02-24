cf7ac83eac852347abd2467744a9306725a7718b
Alonza0314 <dufengplus@gmail.com>
2025-02-24 13:41:11 +0000

    feat: add flow level charging test

--- test/goTest/ulclTrafficInfluence_test.go
+++ test/goTest/ulclTrafficInfluence_test.go
@@ -40,6 +40,11 @@ func TestULCLTrafficInfluence(t *testing.T) {
 		pingN6gwSuccessMecFailed(t)
 	})
 
+	// flow level ping
+	t.Run("Flow Level Ping", func(t *testing.T) {
+		pingOneOneOneOne(t)
+	})
+
 	// check charging record
 	t.Run("Check Charging Record", func(t *testing.T) {
 		checkChargingRecord(t)
@@ -65,6 +70,13 @@ func pingN6gwFailedMecSuccess(t *testing.T) {
 	err = pinger.Pinger(MEC_IP, NIC)
 	if err == nil {
 		t.Errorf("Ping mec failed: expected ping sucess, but got %v", err)
+	}
+}
+
+func pingOneOneOneOne(t *testing.T) {
+	err := pinger.Pinger(ONE_IP, NIC)
+	if err != nil {
+		t.Errorf("Ping one.one.one.one failed: expected ping success, but got %v", err)
 	}
 }
 
@@ -107,7 +119,23 @@ func checkChargingRecord(t *testing.T) {
 		return
 	}
 
-	if chargingRecords[0]["TotalVol"].(float64) == 0 {
-		t.Error("Charging record is empty")
+	t.Run("Check Session Level Charging Record", func(t *testing.T) {
+		checkXLevelChargingRecord(t, chargingRecords, "Session", "")
+	})
+
+	t.Run("Check Flow Level Charging Record", func(t *testing.T) {
+		checkXLevelChargingRecord(t, chargingRecords, "Flow", "internet")
+	})
+}
+
+func checkXLevelChargingRecord(t *testing.T, chargingRecords []map[string]interface{}, level string, dnn string) {
+	for _, record := range chargingRecords {
+		if record["Dnn"] == dnn {
+			if record["TotalVol"].(float64) != 0 {
+				return
+			}
+			t.Errorf("%s level charging record is empty", level)
+		}
 	}
+	t.Errorf("No %s level charging record found", level)
 }
